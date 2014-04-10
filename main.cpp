57bebc90bdcda067dbbfd40fa00337aafa9b5aa2
unknown <melom>
2014-04-09 21:44:19 -0400

    Hello Triangle thanks to http://antongerdelan.net/opengl/hellotriangle.html

--- main.cpp
+++ main.cpp
@@ -1,57 +1,108 @@
-#include "GLFW/glfw3.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 
-static void error_callback(int error, const char* description)
-{
-    fputs(description, stderr);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-int main(void)
-{
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-        
-    window = glfwCreateWindow(640, 480, "Engine", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+int main () {
+  // start GL context and O/S window using the GLFW helper library
+  if (!glfwInit ()) {
+    fprintf (stderr, "ERROR: could not start GLFW3\n");
+    getch();
+    return 1;
+  } 
+
+	// uncomment these lines if on Apple OS X
+  /*glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+
+  GLFWwindow* window = glfwCreateWindow (640, 480, "Hello Triangle", NULL, NULL);
+  if (!window) {
+    fprintf (stderr, "ERROR: could not open window with GLFW3\n");
+    getch();
+    
     glfwTerminate();
-    exit(EXIT_SUCCESS);
+    return 1;
+  }
+  glfwMakeContextCurrent (window);
+                                  
+  // start GLEW extension handler
+  glewExperimental = GL_TRUE;
+  glewInit ();
+
+  // get version info
+  const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
+  const GLubyte* version = glGetString (GL_VERSION); // version as a string
+  printf ("Renderer: %s\n", renderer);
+  printf ("OpenGL version supported %s\n", version);
+
+  // tell GL to only draw onto a pixel if the shape is closer to the viewer
+  glEnable (GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc (GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+  float points[] = {
+   0.0f,  0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+  -0.5f, -0.5f,  0.0f
+  };
+  
+  unsigned int vbo = 0;
+  glGenBuffers (1, &vbo);
+  glBindBuffer (GL_ARRAY_BUFFER, vbo);
+  glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (float), points, GL_STATIC_DRAW);
+  
+  unsigned int vao = 0;
+  glGenVertexArrays (1, &vao);
+  glBindVertexArray (vao);
+  glEnableVertexAttribArray (0);
+  glBindBuffer (GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  const char* vertex_shader =
+  "#version 400\n"
+  "in vec3 vp;"
+  "void main () {"
+  "  gl_Position = vec4 (vp, 1.0);"
+  "}";
+
+  const char* fragment_shader =
+  "#version 400\n"
+  "out vec4 frag_colour;"
+  "void main () {"
+  "  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+  "}";
+  
+  unsigned int vs = glCreateShader (GL_VERTEX_SHADER);
+  glShaderSource (vs, 1, &vertex_shader, NULL);
+  glCompileShader (vs);
+  unsigned int fs = glCreateShader (GL_FRAGMENT_SHADER);
+  glShaderSource (fs, 1, &fragment_shader, NULL);
+  glCompileShader (fs);
+
+  unsigned int shader_programme = glCreateProgram ();
+  glAttachShader (shader_programme, fs);
+  glAttachShader (shader_programme, vs);
+  glLinkProgram (shader_programme);
+
+  while (!glfwWindowShouldClose (window)) {
+    // wipe the drawing surface clear
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram (shader_programme);
+    glBindVertexArray (vao);
+  
+    // draw points 0-3 from the currently bound VAO with current in-use shader
+    glDrawArrays (GL_TRIANGLES, 0, 3);
+  
+    // update other events like input handling 
+    glfwPollEvents ();
+  
+    // put the stuff we've been drawing onto the display
+    glfwSwapBuffers (window);
+  }
+  // close GL context and any other GLFW resources
+  glfwTerminate();
+  return 0;
 }
